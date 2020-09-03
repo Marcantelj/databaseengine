a5044cee4d2fe3dc18261881269a25934fbd7759
Will Marcantel <wamarcantel@gmail.com>
2020-09-02 18:48:28 -0700

    Add http server

--- pkg/databaseengine/server.go
+++ pkg/databaseengine/server.go
@@ -1,9 +1,28 @@
 package databaseengine
 
+import(
+	"fmt"
+	"net/http"
+)
+
 type Server struct{
 
 }
 
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HELLO WORLD")
+}
+
 func (s Server) Run() error {
-	return nil
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", serveIndex)
+
+	srv := http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	return srv.ListenAndServe()
 }
